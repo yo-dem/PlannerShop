729c9e38689d4a30b59484cf55199058304f5cdf
Yodema <yodema.luca@gmail.com>
2025-08-30 16:45:43 +0200

    Added PurchaseEditForm

---
+++ PlannerShop/Forms/PurchaseEditForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace PlannerShop.Forms
+{
+    partial class PurchaseEditForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvData = new DataGridView();
+            searchImage = new PictureBox();
+            pnlSearch = new Panel();
+            txtSearch = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dgvData).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)searchImage).BeginInit();
+            pnlSearch.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dgvData
+            // 
+            dgvData.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dgvData.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvData.Location = new Point(356, 117);
+            dgvData.Name = "dgvData";
+            dgvData.RowHeadersWidth = 51;
+            dgvData.Size = new Size(730, 349);
+            dgvData.TabIndex = 6;
+            // 
+            // searchImage
+            // 
+            searchImage.BackColor = Color.White;
+            searchImage.Image = Properties.Resources.searchWhiteImage;
+            searchImage.Location = new Point(8, 10);
+            searchImage.Name = "searchImage";
+            searchImage.Size = new Size(24, 24);
+            searchImage.SizeMode = PictureBoxSizeMode.AutoSize;
+            searchImage.TabIndex = 22;
+            searchImage.TabStop = false;
+            // 
+            // pnlSearch
+            // 
+            pnlSearch.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            pnlSearch.BackColor = Color.White;
+            pnlSearch.BorderStyle = BorderStyle.FixedSingle;
+            pnlSearch.Controls.Add(searchImage);
+            pnlSearch.Controls.Add(txtSearch);
+            pnlSearch.Location = new Point(356, 65);
+            pnlSearch.Margin = new Padding(3, 4, 3, 4);
+            pnlSearch.Name = "pnlSearch";
+            pnlSearch.Size = new Size(730, 45);
+            pnlSearch.TabIndex = 23;
+            // 
+            // txtSearch
+            // 
+            txtSearch.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            txtSearch.BorderStyle = BorderStyle.None;
+            txtSearch.Font = new Font("Corbel", 14F);
+            txtSearch.Location = new Point(43, 7);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(673, 29);
+            txtSearch.TabIndex = 1;
+            txtSearch.TabStop = false;
+            txtSearch.TextAlign = HorizontalAlignment.Center;
+            txtSearch.TextChanged += txtSearch_TextChanged;
+            // 
+            // PurchaseEditForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1098, 561);
+            Controls.Add(pnlSearch);
+            Controls.Add(dgvData);
+            Name = "PurchaseEditForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "PurchaseEditForm";
+            ((System.ComponentModel.ISupportInitialize)dgvData).EndInit();
+            ((System.ComponentModel.ISupportInitialize)searchImage).EndInit();
+            pnlSearch.ResumeLayout(false);
+            pnlSearch.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dgvData;
+        private PictureBox searchImage;
+        private Panel pnlSearch;
+        private TextBox txtSearch;
+    }
+}
---
+++ PlannerShop/Forms/PurchaseEditForm.cs
@@ -0,0 +1,146 @@
+﻿using PlannerShop.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlannerShop.Forms
+{
+    public partial class PurchaseEditForm : Form
+    {
+        String idCliente;
+        public PurchaseEditForm(String IdCliente)
+        {
+            InitializeComponent();
+            this.idCliente = IdCliente;
+
+            dgvData.DataSource = ModelProdotti.getProdotti();
+            SetProductDataGridStructure();
+
+            Utils.SetDataGridStyle(dgvData);
+        }
+
+        void SetProductDataGridStructure()
+        {
+            int fontSize = 10;
+
+            var idProductColumn = dgvData.Columns["IDPRODOTTO"];
+            idProductColumn.DisplayIndex = 0;
+            idProductColumn.Visible = false;
+            idProductColumn.HeaderText = "IDPRODOTTO";
+            idProductColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            idProductColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            idProductColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            idProductColumn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Regular);
+
+            var dataProductColumn = dgvData.Columns["DATA"];
+            dataProductColumn.DisplayIndex = 1;
+            dataProductColumn.Visible = true;
+            dataProductColumn.HeaderText = "DATA";
+            dataProductColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            dataProductColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataProductColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataProductColumn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Regular);
+
+            var marcaProductColumn = dgvData.Columns["MARCA"];
+            marcaProductColumn.DisplayIndex = 2;
+            marcaProductColumn.Visible = true;
+            marcaProductColumn.HeaderText = "MARCA";
+            marcaProductColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            marcaProductColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            marcaProductColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            marcaProductColumn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Regular);
+
+            var descrizioneProductColumn = dgvData.Columns["DESCRIZIONE"];
+            descrizioneProductColumn.DisplayIndex = 3;
+            descrizioneProductColumn.Visible = true;
+            descrizioneProductColumn.HeaderText = "DESCRIZIONE";
+            descrizioneProductColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            descrizioneProductColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            descrizioneProductColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            descrizioneProductColumn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Bold);
+
+            var aliquotaProductColumn = dgvData.Columns["ALIQUOTA"];
+            aliquotaProductColumn.DisplayIndex = 4;
+            aliquotaProductColumn.Visible = true;
+            aliquotaProductColumn.HeaderText = "IVA";
+            aliquotaProductColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            aliquotaProductColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            aliquotaProductColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            aliquotaProductColumn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Regular);
+
+            var qntProductColumn = dgvData.Columns["QNT"];
+            qntProductColumn.DisplayIndex = 5;
+            qntProductColumn.Visible = true;
+            qntProductColumn.HeaderText = "QNT";
+            qntProductColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            qntProductColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            qntProductColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            qntProductColumn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Regular);
+
+            var prezzoNettoProductColumn = dgvData.Columns["PREZZO_NETTO"];
+            prezzoNettoProductColumn.DisplayIndex = 6;
+            prezzoNettoProductColumn.Visible = true;
+            prezzoNettoProductColumn.HeaderText = "PREZZO NETTO";
+            prezzoNettoProductColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            prezzoNettoProductColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            prezzoNettoProductColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            prezzoNettoProductColumn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Regular);
+
+            var prezzoIvatoProductColumn = dgvData.Columns["PREZZO_IVATO"];
+            prezzoIvatoProductColumn.DisplayIndex = 7;
+            prezzoIvatoProductColumn.Visible = true;
+            prezzoIvatoProductColumn.HeaderText = "PREZZO IVATO";
+            prezzoIvatoProductColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            prezzoIvatoProductColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            prezzoIvatoProductColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            prezzoIvatoProductColumn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Regular);
+
+            var noteProductionColumn = dgvData.Columns["NOTE"];
+            noteProductionColumn.DisplayIndex = 8;
+            noteProductionColumn.Visible = true;
+            noteProductionColumn.HeaderText = "NOTE";
+            noteProductionColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            noteProductionColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            noteProductionColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            noteProductionColumn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Bold);
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            if (txtSearch.Text != String.Empty && dgvData.Rows.Count > 0)
+            {
+                {
+                    foreach (DataGridViewRow dgvr in dgvData.Rows)
+                    {
+                        try
+                        {
+                            String data = dgvr.Cells["DATA"]?.Value?.ToString()?.ToLower() ?? String.Empty;
+                            String marca = dgvr.Cells["MARCA"].Value?.ToString()?.ToLower() ?? String.Empty;
+                            String descrizione = dgvr.Cells["DESCRIZIONE"].Value?.ToString()?.ToLower() ?? String.Empty;
+
+                            if (data.Contains(txtSearch.Text.ToLower())
+                                || marca.Contains(txtSearch.Text.ToLower())
+                                || descrizione.Contains(txtSearch.Text.ToLower()))
+                            {
+                                dgvData.ClearSelection();
+                                dgvData.Rows[dgvr.Index].Selected = true;
+                                dgvData.FirstDisplayedScrollingRowIndex = dgvData.SelectedRows[0].Index;
+                                return;
+                            }
+                        }
+                        catch
+                        {
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
