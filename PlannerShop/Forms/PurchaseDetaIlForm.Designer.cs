511a7d5e08855c21729ee00028c7fbc7ab917bf7
yodema <yodema.luca@gmail.com>
2025-12-05 21:19:25 +0100

    WIP Acquisti

---
+++ PlannerShop/Forms/PurchaseDetaIlForm.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace PlannerShop.Forms
+{
+    partial class PurchaseDetaIlForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnlTop = new Panel();
+            btnCancel = new Button();
+            btnOk = new Button();
+            lblSconto = new Label();
+            lblVendita = new Label();
+            lblQnt = new Label();
+            grpPurchaseDetail = new GroupBox();
+            lblEmail = new Label();
+            lblTelefono = new Label();
+            lblIndirizzo = new Label();
+            lblName = new Label();
+            groupBox1 = new GroupBox();
+            txtPrezzoVendita = new TextBox();
+            nudQnt = new NumericUpDown();
+            nudSconto = new NumericUpDown();
+            lblTotaleCalcolato = new Label();
+            lblTotale = new Label();
+            groupBox2 = new GroupBox();
+            lblDescrizione = new Label();
+            lblMarca = new Label();
+            grpPurchaseDetail.SuspendLayout();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)nudQnt).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nudSconto).BeginInit();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnlTop
+            // 
+            pnlTop.BackColor = Color.FromArgb(192, 192, 255);
+            pnlTop.Dock = DockStyle.Top;
+            pnlTop.Location = new Point(0, 0);
+            pnlTop.Name = "pnlTop";
+            pnlTop.Size = new Size(678, 10);
+            pnlTop.TabIndex = 27;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnCancel.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            btnCancel.ForeColor = Color.Red;
+            btnCancel.Location = new Point(12, 302);
+            btnCancel.Margin = new Padding(3, 2, 3, 2);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(432, 68);
+            btnCancel.TabIndex = 29;
+            btnCancel.TabStop = false;
+            btnCancel.Text = "ANNULLA";
+            btnCancel.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnOk
+            // 
+            btnOk.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btnOk.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            btnOk.Image = Properties.Resources.okImage;
+            btnOk.Location = new Point(450, 302);
+            btnOk.Margin = new Padding(3, 2, 3, 2);
+            btnOk.Name = "btnOk";
+            btnOk.Size = new Size(216, 68);
+            btnOk.TabIndex = 30;
+            btnOk.Text = "SALVA MODIFICHE";
+            btnOk.TextAlign = ContentAlignment.BottomCenter;
+            btnOk.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnOk.UseVisualStyleBackColor = true;
+            // 
+            // lblSconto
+            // 
+            lblSconto.AutoSize = true;
+            lblSconto.Location = new Point(6, 21);
+            lblSconto.Name = "lblSconto";
+            lblSconto.Size = new Size(57, 15);
+            lblSconto.TabIndex = 31;
+            lblSconto.Text = "Sconto %";
+            // 
+            // lblVendita
+            // 
+            lblVendita.AutoSize = true;
+            lblVendita.Location = new Point(6, 81);
+            lblVendita.Name = "lblVendita";
+            lblVendita.Size = new Size(96, 15);
+            lblVendita.TabIndex = 33;
+            lblVendita.Text = "Prezzo di vendita";
+            // 
+            // lblQnt
+            // 
+            lblQnt.AutoSize = true;
+            lblQnt.Location = new Point(6, 141);
+            lblQnt.Name = "lblQnt";
+            lblQnt.Size = new Size(53, 15);
+            lblQnt.TabIndex = 35;
+            lblQnt.Text = "Quantità";
+            // 
+            // grpPurchaseDetail
+            // 
+            grpPurchaseDetail.Controls.Add(lblEmail);
+            grpPurchaseDetail.Controls.Add(lblTelefono);
+            grpPurchaseDetail.Controls.Add(lblIndirizzo);
+            grpPurchaseDetail.Controls.Add(lblName);
+            grpPurchaseDetail.Location = new Point(12, 16);
+            grpPurchaseDetail.Name = "grpPurchaseDetail";
+            grpPurchaseDetail.Size = new Size(432, 138);
+            grpPurchaseDetail.TabIndex = 37;
+            grpPurchaseDetail.TabStop = false;
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.Location = new Point(6, 79);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(41, 15);
+            lblEmail.TabIndex = 8;
+            lblEmail.Text = "EMAIL";
+            // 
+            // lblTelefono
+            // 
+            lblTelefono.AutoSize = true;
+            lblTelefono.Location = new Point(6, 64);
+            lblTelefono.Name = "lblTelefono";
+            lblTelefono.Size = new Size(65, 15);
+            lblTelefono.TabIndex = 7;
+            lblTelefono.Text = "TELEFONO";
+            // 
+            // lblIndirizzo
+            // 
+            lblIndirizzo.AutoSize = true;
+            lblIndirizzo.Font = new Font("Segoe UI", 8F, FontStyle.Bold);
+            lblIndirizzo.Location = new Point(6, 49);
+            lblIndirizzo.Name = "lblIndirizzo";
+            lblIndirizzo.Size = new Size(62, 13);
+            lblIndirizzo.TabIndex = 6;
+            lblIndirizzo.Text = "INDIRIZZO";
+            // 
+            // lblName
+            // 
+            lblName.AutoSize = true;
+            lblName.Location = new Point(6, 19);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(42, 15);
+            lblName.TabIndex = 5;
+            lblName.Text = "NOME";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(txtPrezzoVendita);
+            groupBox1.Controls.Add(nudQnt);
+            groupBox1.Controls.Add(nudSconto);
+            groupBox1.Controls.Add(lblTotaleCalcolato);
+            groupBox1.Controls.Add(lblTotale);
+            groupBox1.Controls.Add(lblSconto);
+            groupBox1.Controls.Add(lblVendita);
+            groupBox1.Controls.Add(lblQnt);
+            groupBox1.Location = new Point(450, 16);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(216, 281);
+            groupBox1.TabIndex = 38;
+            groupBox1.TabStop = false;
+            // 
+            // txtPrezzoVendita
+            // 
+            txtPrezzoVendita.Font = new Font("Segoe UI", 18F);
+            txtPrezzoVendita.Location = new Point(6, 99);
+            txtPrezzoVendita.Name = "txtPrezzoVendita";
+            txtPrezzoVendita.Size = new Size(204, 39);
+            txtPrezzoVendita.TabIndex = 41;
+            txtPrezzoVendita.TextChanged += txtPrezzoVendita_TextChanged;
+            txtPrezzoVendita.KeyPress += txtPrezzoVendita_KeyPress;
+            // 
+            // nudQnt
+            // 
+            nudQnt.Font = new Font("Segoe UI", 18F);
+            nudQnt.Location = new Point(6, 159);
+            nudQnt.Maximum = new decimal(new int[] { 1000, 0, 0, 0 });
+            nudQnt.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            nudQnt.Name = "nudQnt";
+            nudQnt.Size = new Size(204, 39);
+            nudQnt.TabIndex = 40;
+            nudQnt.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            nudQnt.ValueChanged += nudQnt_ValueChanged;
+            nudQnt.KeyUp += nudQnt_KeyUp;
+            // 
+            // nudSconto
+            // 
+            nudSconto.Font = new Font("Segoe UI", 18F);
+            nudSconto.Location = new Point(6, 39);
+            nudSconto.Name = "nudSconto";
+            nudSconto.Size = new Size(204, 39);
+            nudSconto.TabIndex = 39;
+            nudSconto.ValueChanged += nudSconto_ValueChanged;
+            nudSconto.KeyUp += nudSconto_KeyUp;
+            // 
+            // lblTotaleCalcolato
+            // 
+            lblTotaleCalcolato.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTotaleCalcolato.Location = new Point(6, 235);
+            lblTotaleCalcolato.Name = "lblTotaleCalcolato";
+            lblTotaleCalcolato.Size = new Size(204, 34);
+            lblTotaleCalcolato.TabIndex = 38;
+            lblTotaleCalcolato.Text = "TOTALE";
+            lblTotaleCalcolato.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblTotale
+            // 
+            lblTotale.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTotale.Location = new Point(6, 201);
+            lblTotale.Name = "lblTotale";
+            lblTotale.Size = new Size(204, 34);
+            lblTotale.TabIndex = 37;
+            lblTotale.Text = "TOTALE";
+            lblTotale.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(lblDescrizione);
+            groupBox2.Controls.Add(lblMarca);
+            groupBox2.Location = new Point(12, 160);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(432, 137);
+            groupBox2.TabIndex = 38;
+            groupBox2.TabStop = false;
+            // 
+            // lblDescrizione
+            // 
+            lblDescrizione.AutoSize = true;
+            lblDescrizione.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            lblDescrizione.Location = new Point(6, 40);
+            lblDescrizione.Name = "lblDescrizione";
+            lblDescrizione.Size = new Size(114, 21);
+            lblDescrizione.TabIndex = 7;
+            lblDescrizione.Text = "DESCRIZIONE";
+            // 
+            // lblMarca
+            // 
+            lblMarca.AutoSize = true;
+            lblMarca.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblMarca.Location = new Point(6, 19);
+            lblMarca.Name = "lblMarca";
+            lblMarca.Size = new Size(67, 21);
+            lblMarca.TabIndex = 5;
+            lblMarca.Text = "MARCA";
+            // 
+            // PurchaseDetaIlForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(678, 381);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(grpPurchaseDetail);
+            Controls.Add(btnCancel);
+            Controls.Add(btnOk);
+            Controls.Add(pnlTop);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "PurchaseDetaIlForm";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "GESTIONE ACQUISTI - DETTAGLIO";
+            TopMost = true;
+            grpPurchaseDetail.ResumeLayout(false);
+            grpPurchaseDetail.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)nudQnt).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nudSconto).EndInit();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnlTop;
+        private Button btnCancel;
+        private Button btnOk;
+        private Label lblSconto;
+        private Label lblVendita;
+        private Label lblQnt;
+        private GroupBox grpPurchaseDetail;
+        private GroupBox groupBox1;
+        private Label lblEmail;
+        private Label lblTelefono;
+        private Label lblIndirizzo;
+        private Label lblName;
+        private GroupBox groupBox2;
+        private Label lblDescrizione;
+        private Label lblMarca;
+        private Label lblTotale;
+        private Label lblTotaleCalcolato;
+        private NumericUpDown nudSconto;
+        private NumericUpDown nudQnt;
+        private TextBox txtPrezzoVendita;
+    }
+}
---
+++ PlannerShop/Forms/PurchaseDetaIlForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlannerShop.Forms
+{
+    public partial class PurchaseDetaIlForm : Form
+    {
+        private DataRow row;
+        private DataTable dtCliente;
+        public PurchaseDetaIlForm(DataRow row, DataTable dtCliente)
+        {
+            InitializeComponent();
+            this.row = row;
+            this.dtCliente = dtCliente;
+
+            lblName.Text = dtCliente.Rows[0]["NOME"].ToString()!.ToUpper() + " " + dtCliente.Rows[0]["COGNOME"].ToString()!.ToUpper();
+            lblIndirizzo.Text = dtCliente.Rows[0]["INDIRIZZO"].ToString();
+            lblTelefono.Text = "Tel. " + dtCliente.Rows[0]["TELEFONO"].ToString();
+            lblEmail.Text = dtCliente.Rows[0]["EMAIL"].ToString();
+
+            txtPrezzoVendita.Text = row["PREZZO_VENDITA"].ToString();
+
+            nudQnt.Maximum = Convert.ToDecimal(row["QNT"]);
+
+            lblMarca.Text = row["MARCA"].ToString();
+            lblDescrizione.Text = row["DESCRIZIONE"].ToString();
+        }
+
+        private void nudSconto_KeyUp(object sender, KeyEventArgs e)
+        {
+            totalCalculator();
+        }
+
+        private void nudSconto_ValueChanged(object sender, EventArgs e)
+        {
+            totalCalculator();
+        }
+
+        private void txtPrezzoVendita_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back && e.KeyChar != ',')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtPrezzoVendita_TextChanged(object sender, EventArgs e)
+        {
+            totalCalculator();
+        }
+
+        private void nudQnt_KeyUp(object sender, KeyEventArgs e)
+        {
+            totalCalculator();
+        }
+
+        private void nudQnt_ValueChanged(object sender, EventArgs e)
+        {
+            totalCalculator();
+        }
+
+        private void totalCalculator()
+        {
+            if (txtPrezzoVendita.Text != "")
+            {
+                if (txtPrezzoVendita.Text.Contains("€"))
+                {
+                    txtPrezzoVendita.Text = txtPrezzoVendita.Text.Replace(" €", "");
+                }
+                decimal prezzoVendita = Convert.ToDecimal(txtPrezzoVendita.Text);
+                decimal qnt = nudQnt.Value;
+                decimal total = prezzoVendita * qnt - (prezzoVendita * nudSconto.Value / 100);
+                lblTotaleCalcolato.Text = total.ToString("F2") + " €";
+            }
+            else
+            {
+                lblTotaleCalcolato.Text = "Totale: 0.00 €";
+            }
+        }
+
+    }
+}
