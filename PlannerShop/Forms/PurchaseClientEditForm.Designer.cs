3fd2ac80d57f8d991ffcb31441e824a7ee3496ee
yodema <yodema.luca@gmail.com>
2025-12-09 00:15:20 +0100

    Aggiunto visualizza acquisti

---
+++ PlannerShop/Forms/PurchaseClientEditForm.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace PlannerShop.Forms
+{
+    partial class PurchaseClientEditForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PurchaseClientEditForm));
+            pnlTop = new Panel();
+            grpCliente = new GroupBox();
+            lblEmail = new Label();
+            lblTelefono = new Label();
+            lblIndirizzo = new Label();
+            lblName = new Label();
+            grpAcquisti = new GroupBox();
+            pnlSearch = new Panel();
+            searchImage = new PictureBox();
+            txtSearch = new TextBox();
+            dgvDataAcquisto = new DataGridView();
+            grpCliente.SuspendLayout();
+            grpAcquisti.SuspendLayout();
+            pnlSearch.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)searchImage).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvDataAcquisto).BeginInit();
+            SuspendLayout();
+            // 
+            // pnlTop
+            // 
+            pnlTop.BackColor = Color.FromArgb(192, 192, 255);
+            pnlTop.Dock = DockStyle.Top;
+            pnlTop.Location = new Point(0, 0);
+            pnlTop.Name = "pnlTop";
+            pnlTop.Size = new Size(948, 10);
+            pnlTop.TabIndex = 27;
+            // 
+            // grpCliente
+            // 
+            grpCliente.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            grpCliente.Controls.Add(lblEmail);
+            grpCliente.Controls.Add(lblTelefono);
+            grpCliente.Controls.Add(lblIndirizzo);
+            grpCliente.Controls.Add(lblName);
+            grpCliente.Location = new Point(10, 15);
+            grpCliente.Margin = new Padding(3, 2, 3, 2);
+            grpCliente.Name = "grpCliente";
+            grpCliente.Padding = new Padding(3, 2, 3, 2);
+            grpCliente.Size = new Size(926, 106);
+            grpCliente.TabIndex = 28;
+            grpCliente.TabStop = false;
+            grpCliente.Text = "CLIENTE";
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.Location = new Point(5, 84);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(41, 15);
+            lblEmail.TabIndex = 4;
+            lblEmail.Text = "EMAIL";
+            // 
+            // lblTelefono
+            // 
+            lblTelefono.AutoSize = true;
+            lblTelefono.Location = new Point(5, 69);
+            lblTelefono.Name = "lblTelefono";
+            lblTelefono.Size = new Size(65, 15);
+            lblTelefono.TabIndex = 3;
+            lblTelefono.Text = "TELEFONO";
+            // 
+            // lblIndirizzo
+            // 
+            lblIndirizzo.AutoSize = true;
+            lblIndirizzo.Font = new Font("Segoe UI", 8F, FontStyle.Bold);
+            lblIndirizzo.Location = new Point(5, 54);
+            lblIndirizzo.Name = "lblIndirizzo";
+            lblIndirizzo.Size = new Size(62, 13);
+            lblIndirizzo.TabIndex = 2;
+            lblIndirizzo.Text = "INDIRIZZO";
+            // 
+            // lblName
+            // 
+            lblName.AutoSize = true;
+            lblName.Location = new Point(5, 24);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(42, 15);
+            lblName.TabIndex = 0;
+            lblName.Text = "NOME";
+            // 
+            // grpAcquisti
+            // 
+            grpAcquisti.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            grpAcquisti.Controls.Add(pnlSearch);
+            grpAcquisti.Controls.Add(dgvDataAcquisto);
+            grpAcquisti.Location = new Point(10, 125);
+            grpAcquisti.Margin = new Padding(3, 2, 3, 2);
+            grpAcquisti.Name = "grpAcquisti";
+            grpAcquisti.Padding = new Padding(3, 2, 3, 2);
+            grpAcquisti.Size = new Size(926, 428);
+            grpAcquisti.TabIndex = 29;
+            grpAcquisti.TabStop = false;
+            grpAcquisti.Text = "ACQUISTI";
+            // 
+            // pnlSearch
+            // 
+            pnlSearch.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            pnlSearch.BackColor = Color.White;
+            pnlSearch.BorderStyle = BorderStyle.FixedSingle;
+            pnlSearch.Controls.Add(searchImage);
+            pnlSearch.Controls.Add(txtSearch);
+            pnlSearch.Location = new Point(6, 21);
+            pnlSearch.Name = "pnlSearch";
+            pnlSearch.Size = new Size(914, 34);
+            pnlSearch.TabIndex = 24;
+            // 
+            // searchImage
+            // 
+            searchImage.BackColor = Color.White;
+            searchImage.Image = Properties.Resources.searchWhiteImage;
+            searchImage.Location = new Point(7, 4);
+            searchImage.Margin = new Padding(3, 2, 3, 2);
+            searchImage.Name = "searchImage";
+            searchImage.Size = new Size(24, 24);
+            searchImage.SizeMode = PictureBoxSizeMode.AutoSize;
+            searchImage.TabIndex = 22;
+            searchImage.TabStop = false;
+            // 
+            // txtSearch
+            // 
+            txtSearch.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            txtSearch.BorderStyle = BorderStyle.None;
+            txtSearch.Font = new Font("Corbel", 14F);
+            txtSearch.Location = new Point(37, 5);
+            txtSearch.Margin = new Padding(3, 2, 3, 2);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(872, 23);
+            txtSearch.TabIndex = 1;
+            txtSearch.TabStop = false;
+            txtSearch.TextAlign = HorizontalAlignment.Center;
+            // 
+            // dgvDataAcquisto
+            // 
+            dgvDataAcquisto.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dgvDataAcquisto.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDataAcquisto.Location = new Point(6, 60);
+            dgvDataAcquisto.Margin = new Padding(3, 2, 3, 2);
+            dgvDataAcquisto.Name = "dgvDataAcquisto";
+            dgvDataAcquisto.RowHeadersWidth = 51;
+            dgvDataAcquisto.Size = new Size(914, 364);
+            dgvDataAcquisto.TabIndex = 7;
+            // 
+            // PurchaseClientEditForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(948, 640);
+            Controls.Add(grpAcquisti);
+            Controls.Add(grpCliente);
+            Controls.Add(pnlTop);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MinimumSize = new Size(964, 679);
+            Name = "PurchaseClientEditForm";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "ACQUISTI CLIENTE";
+            TopMost = true;
+            WindowState = FormWindowState.Maximized;
+            grpCliente.ResumeLayout(false);
+            grpCliente.PerformLayout();
+            grpAcquisti.ResumeLayout(false);
+            pnlSearch.ResumeLayout(false);
+            pnlSearch.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)searchImage).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvDataAcquisto).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnlTop;
+        private GroupBox grpCliente;
+        private Label lblEmail;
+        private Label lblTelefono;
+        private Label lblIndirizzo;
+        private Label lblName;
+        private GroupBox grpAcquisti;
+        private DataGridView dgvDataAcquisto;
+        private Panel pnlSearch;
+        private PictureBox searchImage;
+        private TextBox txtSearch;
+    }
+}
---
+++ PlannerShop/Forms/PurchaseClientEditForm.cs
@@ -0,0 +1,204 @@
+﻿using PlannerShop.Data;
+using System.Data;
+
+namespace PlannerShop.Forms
+{
+    public partial class PurchaseClientEditForm : Form
+    {
+        private string idCliente;
+        private Dictionary<string, Color> groupColors = new Dictionary<string, Color>();
+        private Color[] palette = new Color[]
+        {
+            Color.White,
+            Color.FromArgb(225, 225, 225)
+        };
+
+        public PurchaseClientEditForm(string idCliente)
+        {
+            InitializeComponent();
+            this.idCliente = idCliente;
+
+            loadClienteData();
+
+
+            DataTable dtAcquistiTemp = ModelAcquisti.getAcquistiByIdCliente(idCliente).Copy();
+
+            Utils.SetDataGridStyle(dgvDataAcquisto, false, 40, 40);
+
+            foreach (DataRow item in dtAcquistiTemp.Rows)
+            {
+                item["TIMESTAMP"] = item["TIMESTAMP"].ToString().Split(' ')[0];
+            }
+
+            dgvDataAcquisto.DataSource = dtAcquistiTemp;
+            SetPurchaseDataGridStructure();
+
+            dgvDataAcquisto.RowPrePaint += DgvDataAcquisto_RowPrePaint;
+
+        }
+
+        private void loadClienteData()
+        {
+            DataTable dtCliente = ModelClienti.getClienteById(idCliente);
+            lblName.Text = dtCliente.Rows[0]["NOME"].ToString()!.ToUpper() + " " + dtCliente.Rows[0]["COGNOME"].ToString()!.ToUpper();
+            lblIndirizzo.Text = dtCliente.Rows[0]["INDIRIZZO"].ToString();
+            lblTelefono.Text = "Tel. " + dtCliente.Rows[0]["TELEFONO"].ToString();
+            lblEmail.Text = dtCliente.Rows[0]["EMAIL"].ToString();
+        }
+
+        void SetPurchaseDataGridStructure()
+        {
+            int fontSize = 10;
+
+            var idPurchaseColumn = dgvDataAcquisto.Columns["IDACQUISTO"];
+            idPurchaseColumn.DisplayIndex = 0;
+            idPurchaseColumn.Visible = false;
+            idPurchaseColumn.HeaderText = "IDACQUISTO";
+            idPurchaseColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.NotSet;
+            idPurchaseColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            idPurchaseColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            idPurchaseColumn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Regular);
+
+            var marcaPurchaseColumnn = dgvDataAcquisto.Columns["MARCA"];
+            marcaPurchaseColumnn.DisplayIndex = 1;
+            marcaPurchaseColumnn.Visible = true;
+            marcaPurchaseColumnn.HeaderText = "MARCA";
+            marcaPurchaseColumnn.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            marcaPurchaseColumnn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            marcaPurchaseColumnn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            marcaPurchaseColumnn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Regular);
+
+            var descrizionePurchaseColumnn = dgvDataAcquisto.Columns["DESCRIZIONE"];
+            descrizionePurchaseColumnn.DisplayIndex = 2;
+            descrizionePurchaseColumnn.Visible = true;
+            descrizionePurchaseColumnn.HeaderText = "DESCRIZIONE";
+            descrizionePurchaseColumnn.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            descrizionePurchaseColumnn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            descrizionePurchaseColumnn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            descrizionePurchaseColumnn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Bold);
+
+            var qntPurchaseColumnn = dgvDataAcquisto.Columns["QNT"];
+            qntPurchaseColumnn.DisplayIndex = 3;
+            qntPurchaseColumnn.Visible = true;
+            qntPurchaseColumnn.HeaderText = "QNT";
+            qntPurchaseColumnn.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            qntPurchaseColumnn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            qntPurchaseColumnn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            qntPurchaseColumnn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Regular);
+
+            var prezzoNettoPurchaseColumn = dgvDataAcquisto.Columns["PREZZO_NETTO"];
+            prezzoNettoPurchaseColumn.DisplayIndex = 4;
+            prezzoNettoPurchaseColumn.Visible = false;
+            prezzoNettoPurchaseColumn.HeaderText = "NETTO";
+            prezzoNettoPurchaseColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.NotSet;
+            prezzoNettoPurchaseColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            prezzoNettoPurchaseColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            prezzoNettoPurchaseColumn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Regular);
+
+            var prezzoIvatoPurchaseColumn = dgvDataAcquisto.Columns["PREZZO_IVATO"];
+            prezzoIvatoPurchaseColumn.DisplayIndex = 5;
+            prezzoIvatoPurchaseColumn.Visible = false;
+            prezzoIvatoPurchaseColumn.HeaderText = "IVATO";
+            prezzoIvatoPurchaseColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.NotSet;
+            prezzoIvatoPurchaseColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            prezzoIvatoPurchaseColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            prezzoIvatoPurchaseColumn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Regular);
+
+            var prezzoVenditaPurchaseColumn = dgvDataAcquisto.Columns["PREZZO_VENDITA"];
+            prezzoVenditaPurchaseColumn.DisplayIndex = 6;
+            prezzoVenditaPurchaseColumn.Visible = false;
+            prezzoVenditaPurchaseColumn.HeaderText = "VENDITA";
+            prezzoVenditaPurchaseColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.NotSet;
+            prezzoVenditaPurchaseColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            prezzoVenditaPurchaseColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            prezzoVenditaPurchaseColumn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Regular);
+
+            var totalePurchaseColumn = dgvDataAcquisto.Columns["TOTALE"];
+            totalePurchaseColumn.DisplayIndex = 7;
+            totalePurchaseColumn.Visible = true;
+            totalePurchaseColumn.HeaderText = "TOTALE";
+            totalePurchaseColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            totalePurchaseColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            totalePurchaseColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            totalePurchaseColumn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Regular);
+
+            var dataPurchaseColumn = dgvDataAcquisto.Columns["DATA"];
+            dataPurchaseColumn.DisplayIndex = 8;
+            dataPurchaseColumn.Visible = false;
+            dataPurchaseColumn.HeaderText = "DATA";
+            dataPurchaseColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.NotSet;
+            dataPurchaseColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataPurchaseColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataPurchaseColumn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Regular);
+
+            var idClientPurchaseColumn = dgvDataAcquisto.Columns["IDCLIENTE"];
+            idClientPurchaseColumn.DisplayIndex = 9;
+            idClientPurchaseColumn.Visible = false;
+            idClientPurchaseColumn.HeaderText = "IDCLIENTE";
+            idClientPurchaseColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.NotSet;
+            idClientPurchaseColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            idClientPurchaseColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            idClientPurchaseColumn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Regular);
+
+            var idProdottoPurchaseColumn = dgvDataAcquisto.Columns["IDPRODOTTO"];
+            idProdottoPurchaseColumn.DisplayIndex = 10;
+            idProdottoPurchaseColumn.Visible = false;
+            idProdottoPurchaseColumn.HeaderText = "IDPRODOTTO";
+            idProdottoPurchaseColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.NotSet;
+            idProdottoPurchaseColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            idProdottoPurchaseColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            idProdottoPurchaseColumn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Regular);
+
+            var aliquotaPurchaseColumn = dgvDataAcquisto.Columns["ALIQUOTA"];
+            aliquotaPurchaseColumn.DisplayIndex = 11;
+            aliquotaPurchaseColumn.Visible = false;
+            aliquotaPurchaseColumn.HeaderText = "IVA";
+            aliquotaPurchaseColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.NotSet;
+            aliquotaPurchaseColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            aliquotaPurchaseColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            aliquotaPurchaseColumn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Regular);
+
+            var notePurchaseColumn = dgvDataAcquisto.Columns["NOTE"];
+            notePurchaseColumn.DisplayIndex = 12;
+            notePurchaseColumn.Visible = false;
+            notePurchaseColumn.HeaderText = "NOTE";
+            notePurchaseColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.NotSet;
+            notePurchaseColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            notePurchaseColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            notePurchaseColumn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Bold);
+
+            var timestampPurchaseColumn = dgvDataAcquisto.Columns["TIMESTAMP"];
+            timestampPurchaseColumn.DisplayIndex = 13;
+            timestampPurchaseColumn.Visible = true;
+            timestampPurchaseColumn.HeaderText = "DATA";
+            timestampPurchaseColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            timestampPurchaseColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            timestampPurchaseColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            timestampPurchaseColumn.DefaultCellStyle.Font = new Font("Corbel", fontSize, FontStyle.Bold);
+        }
+
+        private void DgvDataAcquisto_RowPrePaint(object sender, DataGridViewRowPrePaintEventArgs e)
+        {
+            var grid = sender as DataGridView;
+            if (e.RowIndex < 0) return;
+
+            var row = grid.Rows[e.RowIndex];
+            var timestampValue = row.Cells["TIMESTAMP"].Value?.ToString();
+
+            if (string.IsNullOrEmpty(timestampValue))
+                return;
+
+            // Se questo timestamp non ha ancora un colore associato, glielo diamo
+            if (!groupColors.ContainsKey(timestampValue))
+            {
+                Color nextColor = palette[groupColors.Count % palette.Length];
+                groupColors[timestampValue] = nextColor;
+            }
+
+            // Applicazione del colore
+            row.DefaultCellStyle.BackColor = groupColors[timestampValue];
+        }
+
+
+    }
+}
